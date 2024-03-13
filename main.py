1f9c7510f783ae9532e606a93a51d0a49a58f8e3
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-03-13 20:32:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -274,7 +274,7 @@ Direct Link: [Click Here](https://t.me/MaviTerabox_bot?start={uuid})
         )
         if not download:
             return await hm.edit(
-                f"({data['direct_link']}))         
+                direct_link)         
         file = await bot.send_file(
             PRIVATE_CHAT_ID,
             download,
